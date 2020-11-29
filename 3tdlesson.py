3c3164b1f4eb1cafc720c85aa9c4b2b6c879fb61
Kapitan <kapitanav@gmail.com>
2020-11-29 23:14:26 +0200

    count

---
+++ 3tdlesson.py
@@ -0,0 +1,21 @@
+# value = -160
+# if value < 2:
+#     result = -1
+# else:
+#     result = 2
+#
+# print(result)
+#
+# result = -5 if value < 150 else "e[q"
+#
+# print(result)
+
+
+mystr = "testrtdkuhsdfvlihdfbaefbdfbdfbdfby"
+#print(len(mystr))
+# if "t" in mystr:
+#     print("huy")
+#print(f" я строка {mystr},  хуй пизда")
+#x = mystr[1] = "qqq"
+
+print(mystr.count("f"))
