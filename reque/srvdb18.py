74fc7c5bc713580bef78ff31140ae2913b83dca6
Kapitan <kapitanav@gmail.com>
2021-05-20 09:07:51 +0300

    Update srvdb18.py

--- reque/srvdb18.py
+++ reque/srvdb18.py
@@ -4,6 +4,6 @@ h = {
     "accept": "application/json"
 }
 
-r = requests.get("http://srvdb18:5000/api/SimpleFile/GetSystemInfo", params=h)
+r = requests.get("http:///api/SimpleFile/GetSystemInfo", params=h)
 
 print(r.json()[2]['systemName'])
